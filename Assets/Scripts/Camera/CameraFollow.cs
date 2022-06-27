626bfb873c94ec4a1d1b59ef3d4127c9968b2255
Your Name <you@example.com>
2022-06-27 18:54:31 +0700

    w7_t2

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform targetObject;
+	public float smoothing;
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - targetObject.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = targetObject.position + offset;
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
